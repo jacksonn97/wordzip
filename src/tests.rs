ca9bbae49c5daa8def4e42facda86a79ad03b8ff
jacksonn97 <jackson.n.97@proton.me>
2023-11-27 18:19:46 +0300

    Add structure for test in somewhere
    (in src/indexation.rs)

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,12 @@
 
+pub struct WoCh {
+    pub words: Vec<String>,
+    pub chars: Vec<char>,
+}
 
-#![allow(unused)]
-
-use crate::*;
-
+impl WoCh {
+    #[inline]
+    pub fn len(&self) -> (usize, usize) {
+        (self.words.len(), self.chars.len())
+    }
+}
